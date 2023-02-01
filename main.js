aa5ecbe642dfd8f99b6658493dad7355cfdb36ab
cheru123 <124160147+cheru123@users.noreply.github.com>
2023-02-01 18:25:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:"png",
+    png_quality: 100
+})
+camera=document.getElementById("d1");
+Webcam.attach(camera);
+
+function take_snapshot(){
+    Webcam.snap({ 
+        Document.
+    })
+}
